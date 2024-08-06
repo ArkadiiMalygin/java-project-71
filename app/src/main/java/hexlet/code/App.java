d02244a4c0b1eccac259bfa621d1900d3f8eaf48
Arkadii <arkadii.malygin@gmail.com>
2024-08-06 13:15:13 +0300

    less enters

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,18 +9,18 @@ import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checkDiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable<Integer> {
+public final class App implements Callable<Integer> {
 //    @Option(names = {"-V", "--version"}, description = "Print version information and exit.")
 //    String version = "1.0";
 //    @Option(names = {"-h", "--help"}, usageHelp = true, description = "Show this welldone message and exit.")
 //    private boolean helpRequsted = false;
     @Option(names = {"-f", "--format"}, description = "output format [default: stylish]", defaultValue = "stylish")
-    String format;
+    private String format;
 
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
-    String filepath1;
+    private String filepath1;
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
-    String filepath2;
+    private String filepath2;
 
     @Override
     public Integer call() throws Exception {
